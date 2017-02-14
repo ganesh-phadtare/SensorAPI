22ad5bcb767eabd1f80506e28271d908822b66d9
Ganesh Phadtare <ganesh.phadtare@v2solutions.com>
2017-02-14 16:21:26 +0530

    First CheckIn
    First CheckIn

---
+++ SensorAPI/Controllers/ViewController.cs
@@ -0,0 +1,35 @@
+﻿using SensorAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SensorAPI.Controllers
+{
+    [RoutePrefix("view")]
+    public class ViewController : ApiController
+    {
+        private FormRenderService _renderService;
+        public ViewController() : this(new FormRenderService()) { }
+
+        public ViewController(FormRenderService service)
+        {
+            _renderService = service;
+        }
+        [Route("test/{execType}"), HttpPost]
+        public async Task StartUp([FromUri] int execType, [FromBody]Dictionary<string, string> paramSet)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            await _renderService.Fetch(execType, paramSet);
+
+            watch.Stop();
+            //_logger.Info(string.Format("Entire Call for {0} took {1}", form, watch.ElapsedMilliseconds));
+            //return data;
+        }
+    }
+}
---
+++ SensorAPI/Helpers/AppLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorIOT.Helpers
+{
+    public class AppLogger
+    {
+        static string _FileName = "D:/IOTLogger.txt";
+        public static void LogError(Exception ex)
+        {
+            try
+            {
+                string logMessageToLog = string.Concat(">> Error occured : ", ex.Message, Environment.NewLine, ex.StackTrace, Environment.NewLine);
+                File.AppendAllText(_FileName, logMessageToLog);
+            }
+            catch { }
+        }
+
+        public static void LogTimer(Stopwatch watch)
+        {
+            try
+            {
+                string logMessageToLog = string.Concat("Time Taken : ", watch.ElapsedMilliseconds, " MilliSeconds", Environment.NewLine);
+                File.AppendAllText(_FileName, logMessageToLog);
+            }
+            catch { }
+        }
+    }
+}
